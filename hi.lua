4b3dc4742c6ac65777afef38533aaf79e64b5760
tv7775464-netizen <tv7775464@gmail.com>
2026-08-22 11:40:02 +0700

    Add files via upload

---
+++ hi.lua
@@ -0,0 +1,18 @@
+game.StarterGui:SetCore("SendNotification", {
+    Title = "👍🥀🗿🗿🗿",
+    Text = "script make by thanh",
+    Duration = 10,
+
+    Button1 = "Copy",
+    Button2 = "Close",
+
+    Callback = function(button)
+        if button == "Copy" then
+            setclipboard("turbolite.asia")
+        elseif button == "Close" then
+        end
+    end
+})
+ end
+    end
+})
